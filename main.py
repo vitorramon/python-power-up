cb0aca0b744a6ed22931b8523184947862ba06bc
Vitor Ramon <vitorramon.info@gmail.com>
2025-01-14 20:49:21 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Cadastrar uma base de dados com centenas de produtos através de automação
+
+# Passo 1: Abrir o sistema da empresa
+    # Sistema de Referência
+# Passo 2: Fazer login
+# Passo 3: Importar a base de dados dos produtos
+# Passo 4: Cadastrar 1 produto
+# Passo 5: Repetir o passo 4 até acabar todos os produtos
