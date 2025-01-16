406c1d3f8444b428c8f15d4db3c9b39ef1001777
Vitor Ramon <vitorramon.info@gmail.com>
2025-01-15 22:31:57 -0400

    feat: Criando automação para realizar o logon

---
+++ auxiliar.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+
+time.sleep(5)
+
+# Função para descobrir as coordenadas do mouse 
+print(pyautogui.position())
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # pip install pyautogui 
-
 import pyautogui
+
+# Permite manipular o intervalo de execução do código
+import time
 
 # Delay da execução dos comandos
 pyautogui.PAUSE = 1.5
@@ -31,7 +33,22 @@ pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 
 pyautogui.press("enter")
 
+# Pedir para o computador esperar 3 segundos
+time.sleep(3)
+
 # Passo 2: Fazer login
+pyautogui.click(x=824, y=405)
+pyautogui.write("teste@teste.com")
+
+pyautogui.press("tab")
+pyautogui.write("teste-senha")
+
+pyautogui.press("tab")
+pyautogui.press("enter")
+
 # Passo 3: Importar a base de dados dos produtos
+
+
+
 # Passo 4: Cadastrar 1 produto
 # Passo 5: Repetir o passo 4 até acabar todos os produtos
