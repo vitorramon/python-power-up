28dda534ccf12077fd2669490b4b7a2930bd074f
Vitor Ramon <vitorramon.info@gmail.com>
2025-01-15 21:57:37 -0400

    feat: Criando automação para acessar o navegador

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+# pip install pyautogui 
+
+import pyautogui
+
+# Delay da execução dos comandos
+pyautogui.PAUSE = 1.5
+
+# pyautogui.click -> clicar
+# pyautogui.press -> pressionar uma tecla
+# pyautogui.write -> escrever
+# pyautogui.hotkey -> atalho do teclado
+
 # Cadastrar uma base de dados com centenas de produtos através de automação
 
 # Passo 1: Abrir o sistema da empresa
-    # Sistema de Referência
+    # Sistema de Referência: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+# Entrar no Google Chrome
+
+# Apertar a tecla win
+pyautogui.press("win")
+
+# Digitar o navegador
+pyautogui.write("chrome")
+
+# Apertar enter
+pyautogui.press("enter")
+
+# Entrar no link
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+
+pyautogui.press("enter")
+
 # Passo 2: Fazer login
 # Passo 3: Importar a base de dados dos produtos
 # Passo 4: Cadastrar 1 produto
